eb55974fe1a2908213c9e5549dec27fb72d33d79
li <li@goldragon.criome.net>
2026-05-19 15:50:47 +0200

    signal-repository-ledger: add nix flake checks

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "signal-repository-ledger — ordinary repository ledger Signal contract";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      fenix,
+      crane,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        toolchain = fenix.packages.${system}.stable.withComponents [
+          "cargo"
+          "rustc"
+          "rustfmt"
+          "clippy"
+          "rust-analyzer"
+          "rust-src"
+        ];
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+        src = craneLib.cleanCargoSource ./.;
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      in
+      {
+        packages.default = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
+
+        checks = {
+          build = craneLib.cargoBuild (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+
+          test = craneLib.cargoTest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+
+          fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "signal-repository-ledger";
+          packages = [
+            pkgs.jujutsu
+            toolchain
+          ];
+        };
+      }
+    );
+}
