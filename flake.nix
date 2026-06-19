14f08be87806706f2eacba6938fd4f6a96efb008
li <li@goldragon.criome.net>
2026-06-19 13:47:31 +0200

    use rust-build source cleaning

--- flake.nix
+++ flake.nix
@@ -4,35 +4,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix";
+    rust-build = {
+      url = "github:LiGoldragon/rust-build";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
-      fenix,
-      crane,
+      rust-build,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        toolchain = fenix.packages.${system}.stable.withComponents [
-          "cargo"
-          "rustc"
-          "rustfmt"
-          "clippy"
-          "rust-analyzer"
-          "rust-src"
-        ];
-        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
-        src = craneLib.cleanCargoSource ./.;
+        rust = rust-build.lib.${system}.fromPkgs pkgs;
+
+        inherit (rust) craneLib toolchain;
+        src = rust.cleanCargoSource ./.;
         commonArgs = {
           inherit src;
           strictDeps = true;
